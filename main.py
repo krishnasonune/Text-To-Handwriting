2a8c3354e80ffc4ab2325fd8eee7705f0d685c32
Krishna <krishnasonune87@gmail.com>
2021-08-25 11:16:59 +0530

    text to writing

---
+++ main.py
@@ -0,0 +1,10 @@
+import pywhatkit as kit     #import pywhatkit package
+
+#call text to writing function and pass 3 parameters
+                # 1-text material you want to convert
+                #2-path+image.png
+                #3-rgb code in decimal format to change font color
+kit.text_to_handwriting("hello sir i have done your work in writing",
+                        "C://Users//username//pplus.png",rgb=(0,0,0))
+
+print("your work is done")
